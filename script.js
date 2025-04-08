752be21ebea29fd3e83928e9db4d1726e37497e0
Ahtisham Azhar <ahtishamazhar2003@gmail.com>
2025-04-08 22:59:45 +0500

    set up js file and query selector

---
+++ script.js
@@ -0,0 +1,37 @@
+let box0=document.querySelector("#id0");
+let box1=document.querySelector("#iid1");
+let box2=document.querySelector("#id2");
+let box3=document.querySelector("#id3");
+let box4=document.querySelector("#id4");
+let box5=document.querySelector("#id5");
+let box6=document.querySelector("#id6");
+let box7=document.querySelector("#id7");
+let box8=document.querySelector("#id8");
+
+box0.onclick = ()=> {
+    console.log("Box 0 clicked!");
+};
+box1.onclick = ()=> {
+    console.log("Box 1 clicked!");
+};
+box2.onclick = ()=> {
+    console.log("Box 2 clicked!");
+};
+box3.onclick = ()=> {
+    console.log("Box 3 clicked!");
+};
+box4.onclick = ()=> {
+    console.log("Box 4 clicked!");
+};
+box5.onclick = ()=> {
+    console.log("Box 5 clicked!");
+};
+box6.onclick = ()=> {
+    console.log("Box 6 clicked!");
+};
+box7.onclick = ()=> {
+    console.log("Box 7 clicked!");
+};
+box8.onclick = ()=> {
+    console.log("Box 8 clicked!");
+};
