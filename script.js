5e773c0eb6cab73f27f0390a84381e64c97a30c0
Ahtisham Azhar <ahtishamazhar2003@gmail.com>
2025-04-10 00:31:25 +0500

    dispaly game turn on click

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 let boxes=document.querySelectorAll(".box");
+turnReturn=0;
 
+boxes.forEach((box)=>{
+ box.addEventListener("click",()=>{
+    console.log("clicked");
+    if(turnReturn ===0){
+        turn="X";
+    }
+    else{
+        turn="O";
+    }
+    box.innerText=turn ;
+    turnReturn ++;
+    turnReturn=turnReturn%2;  
+})  
+})
+
+const winningPattern=[
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+];
